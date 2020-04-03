95461f14e9118e62d638a6120586752146e93100
Ian Ramzy <ianramzy@gmail.com>
2020-04-03 09:48:40 -0400

    remove trailing slashes

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ const url = require("url"); // built-in utility
 // enable ssl redirect
 app.use(sslRedirect());
 
+app.use(function (req, res, next) {
+  if (req.path.substr(-1) === "/" && req.path.length > 1) {
+    let query = req.url.slice(req.path.length);
+    res.redirect(301, req.path.slice(0, -1) + query);
+  } else {
+    next();
+  }
+});
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(public, "landing.html"));
 });
